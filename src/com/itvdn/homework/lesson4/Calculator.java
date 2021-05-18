97f19534276cbc8b990dda94d5e9a6912ceccd64
Ivan Lapshov <ilapshov@yandex.ru>
2021-05-18 14:04:37 +0300

    Initial commit

---
+++ src/com/itvdn/homework/lesson4/Calculator.java
@@ -0,0 +1,52 @@
+package com.itvdn.homework.lesson4;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        int operand1 = 22;
+        int operand2 = 33;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("введите знак арифметического действия: + - / * ");
+        String arithmeticSine = sc.next();
+
+        switch (arithmeticSine) {
+            case "+": {
+                System.out.println(operand1 + operand2);
+                break;
+            }
+            case "-": {
+                System.out.println(operand1 - operand2);
+                break;
+            }
+            case "*": {
+                System.out.println(operand1 * operand2);
+                break;
+            }
+            case "/": {
+                System.out.println(operand1 / operand2);
+                break;
+            }
+            default: {
+                System.out.println("неправильный ввод");
+            }
+        }
+
+    }
+
+}
+
+
+//    Используя IntelliJ IDEA, создайте класс Calculator.
+//        Напишите программу – консольный калькулятор.
+//        Создайте две переменные с именами operand1 и operand2. Задайте переменным некоторые произвольные значения.
+//        Предложите пользователю ввести знак арифметической операции.
+//        Примите значение, введенное пользователем и поместите его в строковую переменную sign.
+//        Для организации выбора алгоритма вычислительного процесса, используйте переключатель switch.
+//        Выведите на экран результат выполнения арифметической операции.
+//        В случае использования операции деления, организуйте проверку попытки деления на ноль.
+//        И если таковая имеется, то отмените выполнение арифметической операции и уведомите
+//        об ошибке пользователя.
---
+++ src/com/itvdn/homework/lesson4/Interval.java
@@ -0,0 +1,32 @@
+package com.itvdn.homework.lesson4;
+
+import java.util.Scanner;
+
+public class Interval {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("введите число в диапазоне от 0 до 100");
+        int input = sc.nextInt();
+
+        if (input >= 0 && input <= 14){
+            System.out.println("число в интервале [0 - 14]");
+        } else if (input >= 15 && input <= 35){
+            System.out.println("число в интервале [15 - 35]");
+        }else if (input >= 36 && input <= 50){
+            System.out.println("число в интервале [36 - 50]");
+        }else if (input >= 51 && input <= 100){
+            System.out.println("число в интервале [51 - 100]");
+        } else {
+            System.out.println("неверный ввод");
+        }
+
+    }
+}
+
+//    Используя IntelliJ IDEA, создайте класс Interval.
+//        Напишите программу определения, попадает ли указанное пользователем число от 0 до 100
+//        в числовой промежуток [0 - 14] [15 - 35] [36 - 50][50 - 100]. Если да, то укажите,
+//        в какой именно промежуток. Если пользователь указывает число, не входящее ни в один
+//        из имеющихся числовых промежутков, то выводится соответствующее сообщение.
