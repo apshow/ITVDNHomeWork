b522f71148a346d9855d51ce888a63dda90e796c
Ivan Lapshov <ilapshov@yandex.ru>
2021-05-18 14:04:37 +0300

    Initial commit

---
+++ src/com/itvdn/homework/lesson3/Main.java
@@ -0,0 +1,15 @@
+package com.itvdn.homework.lesson3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //int uberflu?;
+        char _Identifier;
+        //byte \u006fIdentifier;
+        //boolean &myVar;
+        String myVariab1le;
+    }
+}
+
+//Задание 5 - в закоменнтированных строках ошибки в написании переменных
+
